88b6b44de61a619b5360a16c60c57615dffd9192
Daniel Brown <dsbrown1331@gmail.com>
2017-05-11 16:40:27 -0500

    fixed test_placement_prediction to take one demo

--- lfd/test_placement_prediction.py
+++ lfd/test_placement_prediction.py
@@ -10,8 +10,8 @@ import nlp_lfd
 user_id = 26      #user to test on 
 exp_id = 1        #experiment id to test on 
 num_demos = 5     #train on demos in range(0,num_demos)
-test_demo_id = 5  #demonstration id for testing, any number in range(num_demos,10)
-train_data = [(u,e) for u in range(25) for e in range(5)]  #training data for nlp system, this trains on first 25 users for all experiments
+test_demo_id = 5  #demonstration id to predict placement position, any number in range(num_demos,10)
+train_data = [(u,e) for u in range(25) for e in range(5)]  #training data made up of (user_id, exp_id) tuples this example trains the nlp system on first 25 users for all experiments
 
 ###example of LfD placement prediction
 #call this method to get list [x,y] for placement
