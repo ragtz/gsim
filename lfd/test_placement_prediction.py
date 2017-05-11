f7fbfa95164033556b94f84196e28bcb423133a8
Daniel Brown <dsbrown1331@gmail.com>
2017-05-11 15:16:52 -0500

    updated git ignore to ignore displacements add code to compute raw [x,y] sim coords for lfd and nlp+lfd

---
+++ lfd/test_placement_prediction.py
@@ -0,0 +1,24 @@
+import yaml
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import lfd
+import nlp_lfd
+
+
+
+user_id = 26      #user to test on 
+exp_id = 1        #experiment id to test on 
+num_demos = 5     #train on demos in range(0,num_demos)
+test_demo_id = 5  #demonstration id for testing, any number in range(num_demos,10)
+train_data = [(u,e) for u in range(25) for e in range(5)]  #training data for nlp system, this trains on first 25 users for all experiments
+
+###example of LfD placement prediction
+#call this method to get list [x,y] for placement
+lfd_location = lfd.getPredictedPlacementLocation(user_id, exp_id, num_demos, test_demo_id)
+print "predicted placement location for lfd is", lfd_location
+
+
+###example of NLP+LfD placement prediction
+nlp_location = nlp_lfd.getPredictedPlacementLocation(train_data, user_id, exp_id, num_demos, test_demo_id)
+print "predicted placement location for lfd is", nlp_location
